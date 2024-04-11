34638f8db858846b55117627dc9566d0ef476b37
DerrickOngoma <Odrumz64@gmail.com>
2024-04-11 12:02:58 +0300

    sending data

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Add your code here
+function submitData(name, email) {
+    let userData = {
+        name: name,
+        email: email
+    };
+
+    let config = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify(userData)
+    };
+
+    return fetch('http://localhost:3000/users', config)
+        .then(response => response.json())
+        .then(data => {
+            const newId = data.id;
+            const p=document.createElement('p');
+            p.innerHTML = `New ID: ${newId}`;
+            document.body.appendChild(p);
+            // document.body.innerHTML += <p>New ID: ${newId}</p>;
+        })
+        .catch(error => {
+            const m=document.createElement("p")
+            m.innerHTML=`Error: ${error.message}`
+            document.body.appendChild(m)
+            // document.body.innerHTML += <p>Error: ${error.message}</p>;
+        });
+ }
